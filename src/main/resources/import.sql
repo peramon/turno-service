1c3cd67b260413f1256631b71be8af30045a1550
Paul Ramon <peramon98.18@gmail.com>
2023-06-01 16:20:48 -0500

    Agreando el docker file y una base en memoria H2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO turno ( nombre_usuario, area,tramite, observacion, usuario, clave) VALUES ('Paul','Traumatologia','Retiro de equipos','ninguna','peramon','pwdperamon');
-INSERT INTO turno ( nombre_usuario, area, usuario, clave) VALUES ('Erique','Fisiatria','kike','pwdkike');
-INSERT INTO turno ( nombre_usuario, area, usuario, clave) VALUES ('Lilia','Nutricion','sasha','pwdsasha');
+INSERT INTO turno ( nombre_usuario, area,tramite, observacion, usuario, clave) VALUES ('Paul','Financiero','Retiro de equipos','ninguna','peramon','pwdperamon');
+INSERT INTO turno ( nombre_usuario, area, usuario, clave) VALUES ('Erique','RRHH','kike','pwdkike');
+INSERT INTO turno ( nombre_usuario, area, usuario, clave) VALUES ('Lilia','Sistemas','sasha','pwdsasha');
