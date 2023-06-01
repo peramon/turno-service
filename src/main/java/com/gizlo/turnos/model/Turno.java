bf184dd0e2c433b3473cf9489286ba749ca1ac73
Paul Ramon <peramon98.18@gmail.com>
2023-06-01 02:02:04 -0500

    Agregando la entidad para los turnos

--- src/main/java/com/gizlo/turnos/model/Turno.java
+++ src/main/java/com/gizlo/turnos/model/Turno.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.gizlo.turnos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
- * @author DELL
+ * @author Paul
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="turno")
 public class Turno {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    @Column(name = "nombre_usuario")
+    private String nombre;
+    @Column(name = "area")
+    private String area;
 }
